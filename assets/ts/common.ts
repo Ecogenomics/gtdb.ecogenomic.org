7b10057f4376ae870a7340b40d60922e544e3e8d
Aaron Mussig <aaronmussig@gmail.com>
2022-06-03 15:55:02 +1000

    feat(fastani): Add heatmap to FastANI calculator.

--- assets/ts/common.ts
+++ assets/ts/common.ts
@@ -73,3 +73,14 @@ export function isDefined(value: any): boolean {
 
 
 export const COLOURS = ['#189b72', '#47188e', '#2e689b', '#e8a4e2', '#ea989c', '#447704', '#840630', '#2c237f', '#e069cc', '#83eaa2', '#56a2b5', '#ea8c99', '#cc3b69', '#e1b7f7', '#fa7bfc', '#8cba03', '#7c13d8', '#bf8e2b', '#32449e', '#66e86a', '#75ff77', '#65228e', '#56d8cd', '#57d15b', '#72e0c6', '#bdf97c', '#f7bbe2', '#f4d83a', '#f9c1b1', '#1ad8bf', '#abf945', '#77f4d7', '#199689', '#774ee0', '#f5bafc', '#910d30', '#8f63ce', '#dbf497', '#a8dded', '#f49adb', '#bc2b37', '#cbc9ff', '#af94dd', '#0e5d7c', '#ba281d', '#020059', '#f7add8', '#f26896', '#b51c38', '#9ab5f4'];
+
+
+
+export function pickTextColorBasedOnBgColorSimple(bgColor: string, lightColor: string, darkColor: string) {
+  const color = (bgColor.charAt(0) === '#') ? bgColor.substring(1, 7) : bgColor;
+  const r = parseInt(color.substring(0, 2), 16); // hexToR
+  const g = parseInt(color.substring(2, 4), 16); // hexToG
+  const b = parseInt(color.substring(4, 6), 16); // hexToB
+  return (((r * 0.299) + (g * 0.587) + (b * 0.114)) > 186) ?
+    darkColor : lightColor;
+}
---
+++ components/d3/D3ScaleSelect.vue
@@ -0,0 +1,137 @@
+<template>
+  <v-select
+    v-model="model"
+    :items="scalePaletteOptions"
+    dense
+    label="Palette"
+    outlined
+  >
+    <template v-slot:selection="{ item, index }">
+      {{ item.text }}
+    </template>
+    <template v-slot:item="{ item }">
+      <v-row no-gutters>
+        <v-col cols="1" style="vertical-align: middle">
+          {{ item.text }}
+        </v-col>
+        <v-col>
+          <template v-if="item.value === 'cividis'">
+            <img :alt="item.value" src="~/assets/images/d3-scale/cividis.png"/>
+          </template>
+          <template v-else-if="item.value === 'inferno'">
+            <img :alt="item.value" src="~/assets/images/d3-scale/inferno.png"/>
+          </template>
+          <template v-else-if="item.value === 'magma'">
+            <img :alt="item.value" src="~/assets/images/d3-scale/magma.png"/>
+          </template>
+          <template v-else-if="item.value === 'plasma'">
+            <img :alt="item.value" src="~/assets/images/d3-scale/plasma.png"/>
+          </template>
+          <template v-else-if="item.value === 'BrBg'">
+            <img :alt="item.value" src="~/assets/images/d3-scale/BrBg.png"/>
+          </template>
+          <template v-else-if="item.value === 'PRGn'">
+            <img :alt="item.value" src="~/assets/images/d3-scale/PRGn.png"/>
+          </template>
+          <template v-else-if="item.value === 'PiYG'">
+            <img :alt="item.value" src="~/assets/images/d3-scale/PiYG.png"/>
+          </template>
+          <template v-else-if="item.value === 'PuOr'">
+            <img :alt="item.value" src="~/assets/images/d3-scale/PuOr.png"/>
+          </template>
+          <template v-else-if="item.value === 'RdBu'">
+            <img :alt="item.value" src="~/assets/images/d3-scale/RdBu.png"/>
+          </template>
+          <template v-else-if="item.value === 'RdGy'">
+            <img :alt="item.value" src="~/assets/images/d3-scale/RdGy.png"/>
+          </template>
+          <template v-else-if="item.value === 'RdYlBu'">
+            <img :alt="item.value" src="~/assets/images/d3-scale/RdYlBu.png"/>
+          </template>
+          <template v-else-if="item.value === 'RdYlGn'">
+            <img :alt="item.value" src="~/assets/images/d3-scale/RdYlGn.png"/>
+          </template>
+          <template v-else-if="item.value === 'Spectral'">
+            <img :alt="item.value" src="~/assets/images/d3-scale/Spectral.png"/>
+          </template>
+          <template v-else-if="item.value === 'Blues'">
+            <img :alt="item.value" src="~/assets/images/d3-scale/Blues.png"/>
+          </template>
+          <template v-else-if="item.value === 'Greens'">
+            <img :alt="item.value" src="~/assets/images/d3-scale/Greens.png"/>
+          </template>
+          <template v-else-if="item.value === 'Greys'">
+            <img :alt="item.value" src="~/assets/images/d3-scale/Greys.png"/>
+          </template>
+          <template v-else-if="item.value === 'Oranges'">
+            <img :alt="item.value" src="~/assets/images/d3-scale/Oranges.png"/>
+          </template>
+          <template v-else-if="item.value === 'Purples'">
+            <img :alt="item.value" src="~/assets/images/d3-scale/Purples.png"/>
+          </template>
+          <template v-else-if="item.value === 'Reds'">
+            <img :alt="item.value" src="~/assets/images/d3-scale/Reds.png"/>
+          </template>
+          <template v-else>
+            <img :alt="item.value" src="~/assets/images/d3-scale/viridis.png"/>
+          </template>
+        </v-col>
+      </v-row>
+    </template>
+  </v-select>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  props: {
+    value: {
+      type: String,
+      default: 'viridis',
+    },
+  },
+  data: () => ({
+    scalePaletteOptions: [
+      {header: "Sequential (Multi-Hue)"},
+      {text: "Viridis", value: "viridis"},
+      {text: "Plasma", value: "plasma"},
+      {text: "Inferno", value: "inferno"},
+      {text: "Magma", value: "magma"},
+      {text: "Cividis", value: "cividis"},
+      {header: "Diverging"},
+      {text: "BrBg", value: "BrBg"},
+      {text: "PRGn", value: "PRGn"},
+      {text: "PiYG", value: "PiYG"},
+      {text: "PuOr", value: "PuOr"},
+      {text: "RdBu", value: "RdBu"},
+      {text: "RdGy", value: "RdGy"},
+      {text: "RdYlBu", value: "RdYlBu"},
+      {text: "RdYlGn", value: "RdYlGn"},
+      {text: "Spectral", value: "Spectral"},
+      {header: "Sequential (Single-Hue)"},
+      {text: "Blues", value: "Blues"},
+      {text: "Greens", value: "Greens"},
+      {text: "Greys", value: "Greys"},
+      {text: "Oranges", value: "Oranges"},
+      {text: "Purples", value: "Purples"},
+      {text: "Reds", value: "Reds"},
+    ],
+    scalePalette: "viridis",
+  }),
+
+  computed: {
+    // Listen to / update the bound model
+    model: {
+      get(): string {
+        return this.value;
+      },
+      set(value: string) {
+        this.$emit('input', value);
+      },
+    },
+
+  },
+  methods: {}
+})
+</script>
