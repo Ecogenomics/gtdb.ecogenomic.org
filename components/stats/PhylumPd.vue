406dba691c79d47cbefd9d5c00386ecd25941002
Aaron Mussig <aaronmussig@gmail.com>
2024-08-12 09:43:23 +1000

    feat(stats): Add PhylumPD to the stats page.

---
+++ components/stats/PhylumPd.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-data-table
+    :headers="headers"
+    :items="rows"
+    class="gtdb-table"
+    dense
+    item-key="taxon"
+  >
+  </v-data-table>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+
+export default Vue.extend({
+  props: {
+    rows: {
+      type: Array,
+      required: true
+    },
+  },
+  data() {
+    return {
+      headers: [
+        {text: 'Phylum', value: 'taxon'},
+        {text: 'Scaled (%)', value: 'scaled'},
+        {text: 'Unscaled (%)', value: 'unscaled'},
+        {text: 'Abs. difference', value: 'abs'}
+      ]
+    }
+  }
+})
+</script>
+
+<style scoped>
+</style>
--- components/stats/REDr220.vue
+++ components/stats/REDr220.vue
@@ -16,38 +16,38 @@ import * as d3 from 'd3v5';
 //@ts-ignore
 import mpld3 from 'mpld3';
 
-const archaea = require('~/assets/data/stats/r214/red-archaea.json')
-const bacteria = require('~/assets/data/stats/r214/red-bacteria.json')
+// const archaea = require('~/assets/data/stats/r214/red-archaea.json')
+// const bacteria = require('~/assets/data/stats/r214/red-bacteria.json')
 
 export default Vue.extend({
-  mounted() {
-
-    mpld3.register_plugin("axisreplacer", AxisReplacer);
-    AxisReplacer.prototype = Object.create(mpld3.Plugin.prototype);
-    AxisReplacer.prototype.constructor = AxisReplacer;
-    AxisReplacer.prototype.requiredProps = ["data"];
-    AxisReplacer.prototype.defaultProps = {}
-    // @ts-ignore
-    function AxisReplacer(fig, props){
-      // @ts-ignore
-      mpld3.Plugin.call(this, fig, props);
-    }
-
-    AxisReplacer.prototype.draw = function(){
-      let data = this.props.data;
-      let parent = document.getElementsByClassName("mpld3-yaxis")[0];
-      let gTicks = parent.getElementsByTagName("g");
-      for (let i=0; i < gTicks.length; i++) {
-        let curTick = gTicks[i];
-        let curText = curTick.getElementsByTagName("text")[0];
-        curText.innerHTML = data[i];
-      }
-    };
-
-    // @ts-ignore
-    mpld3.draw_figure("mpld3_archaea_r214", archaea);
-    mpld3.draw_figure("mpld3_bacteria_r214", bacteria);
-  }
+  // mounted() {
+  //
+  //   mpld3.register_plugin("axisreplacer", AxisReplacer);
+  //   AxisReplacer.prototype = Object.create(mpld3.Plugin.prototype);
+  //   AxisReplacer.prototype.constructor = AxisReplacer;
+  //   AxisReplacer.prototype.requiredProps = ["data"];
+  //   AxisReplacer.prototype.defaultProps = {}
+  //   // @ts-ignore
+  //   function AxisReplacer(fig, props){
+  //     // @ts-ignore
+  //     mpld3.Plugin.call(this, fig, props);
+  //   }
+  //
+  //   AxisReplacer.prototype.draw = function(){
+  //     let data = this.props.data;
+  //     let parent = document.getElementsByClassName("mpld3-yaxis")[0];
+  //     let gTicks = parent.getElementsByTagName("g");
+  //     for (let i=0; i < gTicks.length; i++) {
+  //       let curTick = gTicks[i];
+  //       let curText = curTick.getElementsByTagName("text")[0];
+  //       curText.innerHTML = data[i];
+  //     }
+  //   };
+  //
+  //   // @ts-ignore
+  //   mpld3.draw_figure("mpld3_archaea_r214", archaea);
+  //   mpld3.draw_figure("mpld3_bacteria_r214", bacteria);
+  // }
 })
 </script>
 
